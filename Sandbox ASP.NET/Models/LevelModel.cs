62ba61f60756e8d3506f9097a6319b1318e5ca2d
StrangePerch <sasha.aav.sv@gmail.com>
2021-07-19 18:57:13 +0300

    LevelViewModel renamed to LevelModel

---
+++ Sandbox ASP.NET/Models/LevelModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sandbox_ASP.NET.Models
+{
+    public class LevelModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "NameRequired"), Display(Name = "Name")]
+        [StringLength(30, ErrorMessage = "NameLength", MinimumLength = 3)]
+        public string Name { get; set; }
+        [Display(Name = "Author")]
+        public string Author { get; set; }
+        [Display(Name = "Public")]
+        public bool Public { get; set; }
+        [Required(ErrorMessage = "MapRequired"), Display(Name = "Map")]
+        public string Map { get; set; }
+    }
+}
